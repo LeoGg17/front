6e2344924dbb5d940f0234eb816cc7cd8a503b26
LeoGg17 <leoguartambel777@gmail.com>
2022-03-17 22:49:06 -0500

    2

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 export class ProductService {
-
-    // baseUrl = "http://localhost:8080/api/products/";
-    baseUrl = "https://hg-rest-api.herokuapp.com/api/products/";
+ 
+    baseUrl = "https://crudleoguartambel.herokuapp.com/api/productos/";
 
     create(product){
         return axios.post(this.baseUrl+"product/", product).then(res => res.data);
